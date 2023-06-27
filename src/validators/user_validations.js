b456462e5c03dfdf325c020ed962f8e75073cc6d
Norman Arredondo <arredondo_norman25@outlook.es>
2023-06-26 22:17:14 -0600

    Validators

--- src/validators/user_validations.js
+++ src/validators/user_validations.js
@@ -3,6 +3,6 @@ import {check} from "express-validator";
 export const new_user_validation = [
     //Validaciones para campos para post
     check('user_name', 'El campo user_name es obligatorio').not().isEmpty(),
-    check('user_password', 'El password deve ser mínimo de 6 caracteres').isLength({min:6}),
+    check('user_password', 'El password debe ser mínimo de 6 caracteres').isLength({min:6}),
     check('user_email', 'Ingresa un email valido').isEmail()
 ];
