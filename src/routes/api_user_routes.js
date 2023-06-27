8063b6ce9a1a633fb43d8c55267cf7c9992d9b1c
Norman Arredondo <arredondo_norman25@outlook.es>
2023-06-26 22:14:55 -0600

    routes

--- src/routes/api_user_routes.js
+++ src/routes/api_user_routes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { new_user_validation } from "../validators/user_validations.js";
-import {new_user } from "../controllers/user_controller.js";
+import { new_user } from "../controllers/user_controller.js";
 
 const api_user_router = express.Router();
 
-api_user_router.get("/", (req, res, next)=> {
+api_user_router.get("/", (req, res, next) => {
     res.send("usuarios");
 });
 
-api_user_router.get("/:id", (req, res, next)=> {
-    const {params} = req; //req es un arreglo enorme
-    console.log("params:" , params);
+api_user_router.get("/:id", (req, res, next) => {
+    const { params } = req; //req es un arreglo enorme
+    console.log("params:", params);
     res.send(`Ver usuario ${params.id}`);
 });
 
