e01d4a00cb094534aea6dd0f4cce6026058a7c3a
Norman Arredondo <arredondo_norman25@outlook.es>
2023-07-03 20:07:16 -0600

    Login
    Funcionalidad

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -1,103 +1,167 @@
-//Variblaes 
-const formulario = document.querySelector('#formulario');
-const input_email = document.querySelector('#input_email');
-const password = document.querySelector('#input_password');
+//Variables 
 
-let u_email = [];
-let u_password = [];
-
-//Event Listeners 
-eventListeners();
-
-function eventListeners() {
-    formulario.addEventListener('submit', agregarDatos);
-
-    //Cuando el documento está listo
-    document.addEventListener('DOMContentLoaded', () => {
-        //[] si no hay nada en localstorage se crea un arreglo vacío
-        u_email = JSON.parse(localStorage.getItem('u_email')) || [];
-        u_password = JSON.parse(localStorage.getItem('u_password')) || [];
-        console.log(u_email);
-    });
-
-}
+const password_input = document.querySelector("#password_input");
+const email_input = document.querySelector("#email_input");
+const show_password_cb = document.querySelector("#show_password_cb");
+const send_btn = document.querySelector("#send_btn");
+const mensaje_password = document.querySelector("#mensaje_password");
+const mensaje_correo = document.querySelector("#mensaje_correo");
+const iconExclamation = document.querySelector("#iconExclamation");
+const iconGood = document.querySelector("#iconGood");
 
 
-//Funciones
-function agregarDatos(e) {
-    e.preventDefault();
-
-    //Label donde el usuario escribe
-    const input_email = document.querySelector('#input_email').value;
-    const input_password = document.querySelector('#input_password').value;
-
-
-    //Validación
-    if (input_email === '' || input_password === '') {
-        mostrarError('Todos los campos son obligatorios');
-        return;
+//Funciones con Arrow Funcions
+const show_password = (e) => {
+    //e.target.checked -> regresa true o false
+    if(e.target.checked){
+        //Si el checkbox seleccionado es true
+        password_input.setAttribute("type", "text"); //lo pasa a texto
+    }else {
+        password_input.setAttribute("type", "password");
     }
+} 
 
-    //Añadir al arreglo la información de los input
-    u_email = [...u_email, input_email];
-    u_password = [...u_password, input_password];
-    
-
-    console.log('Se agregó el usuario y contraseña');
-
-    //Funcion para guardar los datos en LocalStorage
-    sincronizarStorage();
-
-    //Limpiar formulario
-    formulario.reset();
-
-}
-
-function mostrarError(error) {
-    const mensajeError = document.createElement('p');
-    mensajeError.textContent = error;
-    mensajeError.classList.add('error');
-
-    //Definir dónde va a salir el mensaje
-    //Insertarlo
-    const contenido = document.querySelector('#contenido');
-    contenido.appendChild(mensajeError);
-
-    setTimeout(() => {
-        mensajeError.remove();
-    }, 2000);
-}
-
-//Agrega los datos a LocalStorage
-function sincronizarStorage() {
-    localStorage.setItem('u_email', JSON.stringify(u_email));
-    localStorage.setItem('u_password', JSON.stringify(u_password));
-}
-
-
-
-
-const login = async (e) => {
+/*
+const enviar = async (e) => {
     try {
-        const data = {
-            user_email: "miguel@miguel.com",
-            user_password: "123456"
+        e.preventDefault();
+        if(
+            email_input.value.trim() === "" ||
+            password_input.value.trim() === ""
+        ){
+            mensaje_password.innerText = "Los campos son obligatorios"
+            mensaje_password.classList.add("has-text-weight-bold")
+            setTimeout(() => {
+                mensaje_password.classList.remove("has-text-weight-bold")
+            }, 1000);
+            return;
         }
-        const response = await fetch("http://localhost:3001/api/user/login/", {
+
+        const data = {
+            user_email: email_input.value,
+            user_password: password_input.value
+        }
+
+        const response = await fetch("http://localhost:3001/api/user/login", {
             method: "POST",
             headers: {
-                'Content-Type': "application/x-www-form-urlencoded;charset=UTF-8"
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: new URLSearchParams(data)
+        })
+
+        const result = await response.json();
+
+        const {error} = result;
+        if(error){
+            mensaje_password.innerText = error;
+            mensaje_password.classList.add("has-text-weight-bold")
+            setTimeout(() => {
+                mensaje_password.classList.remove("has-text-weight-bold")
+            }, 1000);
+            return;
+        }else{
+            mensaje_password.innerText = "";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+} 
+*/
+
+const enviar = async(e) => {
+    try {
+        e.preventDefault();
+
+        //Validar los campos de email y password al mismo tiempo
+        if(
+            email_input.value.trim() === "" || password_input.value.trim() === ""
+            ){
+            mensaje_password.innerText = "Los campos son obligatorios"
+            //Poner el input en negritas
+            mensaje_password.classList.add("has-text-weight-bold")
+            email_input.classList.add("is-danger");
+            setTimeout(() => {
+                mensaje_password.classList.remove("has-text-weight-bold")
+            }, 1000);
+            return; //Para que no se ejecute lo demás si no pasa
+        }
+
+        //Si pasa la validación
+        const data = {
+            user_email: email_input.value,
+            user_password: password_input.value
+        }
+
+        //Comunicamos a la vista con el API
+        const response = await fetch("http://localhost:3001/api/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
             },
             body: new URLSearchParams(data)
         });
 
         const result = await response.json();
-        console.log(result)
+        console.log('result',result)
 
-    } catch (error) {
+        const {error, errores, token} = result; //usuario no registrado y contraseña
+        
+        if(errores){
+            errores.forEach(err =>{
+                if(err.path === "user_email"){
+                    mensaje_correo.innerText = err.msg
+                    //Poner el input en negritas
+                    mensaje_correo.classList.add("has-text-weight-bold");
+                    email_input.classList.add("is-danger");
+                    setTimeout(() => {
+                        mensaje_correo.classList.remove("has-text-weight-bold");
+                        email_input.classList.remove("is-danger");
+                    }, 1000);
+                    return; //Para que no se ejecute lo demás si no pasa
+                }
+
+                if(err.path === "user_password"){
+                    mensaje_password.innerHTML = err.msg;
+                    mensaje_password.classList.add("has-text-weight-bold");
+                    password_input.classList.add("is-danger");
+                    console.log("user_password")
+                    
+                    setTimeout(() => {
+                        mensaje_password.classList.remove("has-text-weight-bold");
+                        password_input.classList.remove("is-danger");
+                    }, 1000);
+                    return;
+                }
+            });
+        }
+        if(error){
+            mensaje_password.innerText = error;
+            mensaje_password.classList.add("has-text-weight-bold")
+            password_input.classList.add("is-danger");
+            setTimeout(() => {
+                mensaje_password.classList.remove("has-text-weight-bold")
+                password_input.classList.remove("is-danger");
+            }, 1000);
+            return;
+        }else{
+            mensaje_password.innerText = "";
+
+            if(token){
+                email_input.classList.add("is-success");
+                password_input.classList.add("is-success");
+                setTimeout(() => {
+                    // alert("usuario logueado")
+                    window.location.replace("http://localhost:3001/");
+                }, 1000);
+            }
+        }
+
+    } catch(error) {
         console.log(error)
     }
 }
 
-document.addEventListener("DOMContentLoaded", login);
-
+//Events
+show_password_cb.addEventListener("click", show_password);
+send_btn.addEventListener("click", enviar);
