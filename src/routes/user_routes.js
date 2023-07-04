b964c284360c9366d7bc8aaf6b891bb59faaa475
Norman Arredondo <arredondo_norman25@outlook.es>
2023-07-03 20:10:41 -0600

    Modificando nombre
    imports

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,13 +1,17 @@
 import express from "express";
+import {session_validation} from "../validators/session_validation.js";
+import { users_view, prueba_view } from "../controllers/user_controller.js";
 
-const user_router = express.Router();
-user_router.get("/", (req, res, next) => {
-    res.send("Usuarios");
-});
+//Para renderizar las vistas
+const user_routes = express.Router();
 
-user_router.get("/:id", (req, res, next) => {
+//Get para recibir vistas 
+user_routes.get("/users", session_validation, users_view);
+user_routes.get("/prueba", session_validation, prueba_view);
+
+user_routes.get("/:id", (req, res, next) => {
     const { params } = req;
     res.send('Ver un usuario');
 });
 
-export default user_router;
+export default user_routes;
