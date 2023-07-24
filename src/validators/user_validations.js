b38f51370e3dc9cbc1dc71866f998e0e55d4231f
Norman Arredondo <arredondo_norman25@outlook.es>
2023-07-24 11:14:36 -0600

    Validación
    Contraseña de exactamente 6 caracteres

--- src/validators/user_validations.js
+++ src/validators/user_validations.js
@@ -3,7 +3,7 @@ import { check } from "express-validator";
 export const new_user_validation = [
     //Validaciones para campos para post
     check("user_name", "El campo user_name es obligatorio").not().isEmpty(),
-    check("user_password", "El password debe ser minimo de 6 caracteres").isLength({ min: 6 }),
+    check('user_password', 'La contraseña debe de ser de exactamente 6 caracteres').isLength({ min: 6, max: 6 }),
     check("user_email", "Ingresa un email valido").isEmail()
 ];
 
