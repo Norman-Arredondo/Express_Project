7021c945b0900fa280c6ac70d0c745e3422d6874
Norman Arredondo <arredondo_norman25@outlook.es>
2023-07-28 11:41:20 -0600

    Rutas limpias

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,27 +1,29 @@
 import express from "express";
-import {session_validation} from "../validators/session_validation.js";
-import { users_view, registro_view, registrar, create_user_view, delete_user } from "../controllers/user_controller.js";
+import { session_validation } from "../validators/session_validation.js";
+import { users_view, delete_user_view, edit_user_view, create_new_user_view, registro_view, registrar, } from "../controllers/user_controller.js";
 
 //Para renderizar las vistas
 const user_routes = express.Router();
 //Get para recibir vistas 
 
 
-//Dashboard
-user_routes.get("/users", session_validation, users_view);
+// Vista del Dashboard
+user_routes.get("/dashboard", session_validation, users_view);
 
-//Vista Nuevo Usuario --Corregir
+// Vista  Formulario Eliminar usuario
+user_routes.get('/delete/:user_id', session_validation, delete_user_view);
+
+// Vista Formulario editar Usuario
+user_routes.get('/edit/:user_id', session_validation, edit_user_view);
+
+// Vista para Formulario crear Nuevo Usuario
+user_routes.get('/create/new_user', session_validation, create_new_user_view);
+
+//Vista Nuevo Usuario
 user_routes.get("/registro", registro_view);
 user_routes.post("/registro", registrar);
 
-//Vista Formulario Crear Nuevo Usuario
-user_routes.get("/user/nuevo",create_user_view);
 
-// Envia informacion de un usuario
-user_routes.get('/delete/:user_id', delete_user);
-
-//Formulario para editar un usuario
-user_routes.get("/:id", );
 
 
 export default user_routes;
