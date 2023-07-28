a7ba7f7188f18382015ae2571a5b1af443c0d330
Norman Arredondo <arredondo_norman25@outlook.es>
2023-07-28 11:38:11 -0600

    Eliminar Usuario

---
+++ src/public/js/delete_user.js
@@ -0,0 +1,36 @@
+const send_btn = document.getElementById("send_btn")
+const id_input = document.querySelector("#id_input");
+
+
+const showModal = () => {
+    const modal = document.getElementById('myModal');
+    modal.classList.add('is-active');
+}
+
+const locationReplace = () => {
+    window.location.replace("http://localhost:3001/users/dashboard");
+}
+
+const eliminar = async (e) => {
+    const response = await fetch(`http://localhost:3001/api/user/${id_input.value}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        }
+    })
+
+    const result = await response.json();
+    console.log("result: ", result)
+
+
+    const { respuesta } = result;
+
+    if (respuesta) {
+        showModal();
+    }
+
+
+}
+
+
+send_btn.addEventListener("click", eliminar)
